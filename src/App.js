206df94263cad78ce6e44f59c37b55a4775a1fd4
nlo88 <nloyeltylo@gmail.com>
2023-03-10 17:53:54 -0500

    top bar is up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 //import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
+    <div>
+      <Navbar />
     <h1 className="text-3xl font-bold ">
-      A GOOD SAMARITAN.
+      Selma Blair joins GUIDE Beauty.
     </h1>
+    </div>
+    
+    
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react"
+export default function NavBar() {
+
+return (
+
+  <>
+  
+  <nav>
+    <div class="flex justify-center items-center md:px-8 py-5 bg-yellow-500	background-color: rgb(64 56 43);">
+            <a href="#" class="text-sm font-medium text-white hover:text-grey-700">FREE US SHIPPING ON ORDERS $35+</a>   
+    </div>
+  </nav>
+
+  <nav class="border-gray-200 px-8 sm: px-8 py-2.5">
+    <div class="container flex flex-wrap items-center justify-between mx-auto">
+    <a href="/" class="flex items-center">
+      <span class="self-center text-xl font-bold">GUIDE BEAUTY</span>
+    </a>
+    </div>
+    <div>
+      <button type="button" ></button>
+    </div>
+
+  </nav>
+
+  </>
+    );  
+}
