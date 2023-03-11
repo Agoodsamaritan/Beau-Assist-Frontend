8e08cce738cbae42890e64cd6e17fb3e62669382
nlo88 <nloyeltylo@gmail.com>
2023-03-11 17:47:05 -0500

    tailwind aspect ratio added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -72,7 +72,6 @@ return (
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
         </a>
-      
       </nav>
     
     </section>
---
+++ src/components/Products.js
@@ -0,0 +1,13 @@
+
+
+export default function Products() {
+
+return (
+<>
+
+<h1>TEST</h1>
+
+</>
+
+    );
+}
