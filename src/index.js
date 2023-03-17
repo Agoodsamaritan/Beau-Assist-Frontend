c3a814f45402a6673da563e82e942cf3f10742f3
N-Santiago <norbertosantiago82@gmail.com>
2023-03-16 23:00:13 -0500

    rebase

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './store';
 
-const root = document.getElementById('root');
 
-ReactDOM.createRoot(root).render(
-  <Provider store={store}>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
 
 
