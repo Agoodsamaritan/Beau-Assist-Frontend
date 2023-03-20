6ed33c14f22db827ba7c3c44658cdb53f8fa57c5
nlo88 <nloyeltylo@gmail.com>
2023-03-19 20:59:07 -0400

    sweep and json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Navbar from './compoents/Navbar';
-import Products from './compoents/Products';
-import Product from './compoents/Product';
-import Home from './compoents/Home';
-import Cart from './compoents/Cart';
-import Modal from './compoents/Modal';
-import Footer from './compoents/Footer';
-import Results from './compoents/Results'
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+import Product from './components/Product';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import Modal from './components/Modal';
+import Footer from './components/Footer';
+import Results from './components/Results'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './service/reducers/index'
