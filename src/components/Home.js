a7ed0feba793f8e1d3714f5f16817daef0fabf0f
N-Santiago <norbertosantiago82@gmail.com>
2023-03-21 18:54:41 -0500

    Details in Home and Footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
     
     return (
-      <div className="relative overflow-hidden bg-white">
+      <div className="relative overflow-hidden">
         <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
           <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
             <div className="sm:max-w-lg">
