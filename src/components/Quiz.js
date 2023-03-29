720cfeeb85a0ff00c9bee8abfe8adba17e80eadd
N-Santiago <norbertosantiago82@gmail.com>
2023-03-28 19:39:44 -0500

    Testing how to embed tik tok video to the quiz result

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -40,6 +40,7 @@ export default function Quiz() {
   
   const recommendations = [
     {
+      "id": 1,
       "skinType": "Oily",
       "ageRange": "13-17",
       "concerns": ["Acne"],
@@ -52,6 +53,7 @@ export default function Quiz() {
       ]
     },
     {
+      "id": 2,
       "skinType": "Oily",
       "ageRange": "18-24",
       "concerns": ["Acne"],
@@ -62,8 +64,50 @@ export default function Quiz() {
         "Sunscreen",
         "Retinol serum (PM, alternate nights)"
       ]
-    }  
-  ]
+    },
+    {
+      "id": 3,
+      "skinType": "Dry",
+      "ageRange": "35-44",
+      "concerns": ["Fine lines and wrinkles"],
+      "routine": [
+        "Foaming cleanser",
+        "Hydration toner",
+        "Gentle moisturizer",
+        "Sunscreen",
+        "Extra hydration night cream"
+      ]
+    },
+    {
+      "id": 4,
+      "skinType": "Dry",
+      "ageRange": "45-54",
+      "concerns": ["Fine lines and wrinkles"],
+      "routine": [
+        "Foaming cleanser",
+        "Hydration toner",
+        "Gentle moisturizer",
+        "Sunscreen",
+        "Extra hydration night cream"
+      ]
+    },
+    {
+      "id": 5,
+      "skinType": "Mature",
+      "ageRange": "55+",
+      "concerns": ["Loss of firmness and elasticity"],
+      "routine": [
+        "Cream cleanser",
+        "Hydrating toner",
+        "Rich moisturizer",
+        "Sunscreen",
+        "Firming serum (AM and PM)",
+        "Brightening serum (AM and PM)",
+        "Eye cream (AM and PM)"
+      ]
+    }
+  ];
+  
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showThanks, setShowThanks] = useState(false)
@@ -82,34 +126,45 @@ export default function Quiz() {
     }
   };
 
-    const [skinType, setSkinType] = useState('')
-    const [ageRange, setAgeRange] = useState('')
-    const [skinConcerns, setSkinConcerns] = useState('')
 
-    const handleSkinTypeSelection = (selectedOption) => {
-        setSkinType(selectedOption.value);
-    }
+  const [skinType, setSkinType] = useState('')
+  const [ageRange, setAgeRange] = useState('')
+  const [skinConcerns, setSkinConcerns] = useState('')
 
-    const handleAgeRangeSelection = (selectedOption) => {
-        setAgeRange(selectedOption.value);
-    }
+  const handleSkinTypeSelection = (selectedOption) => {
+      setSkinType(selectedOption.value);
+  }
 
-    const handleSkinConcernsSelection = (selectedOptions) => {
-        const selectedValues = selectedOptions.map((option) => option.value);
-        setSkinConcerns(selectedValues);
-    }
+  const handleAgeRangeSelection = (selectedOption) => {
+      setAgeRange(selectedOption.value);
+  }
 
-    const findRecommendation = () => {
-        const recommendations = recommendations.find((rec) => {
-            
-        })
-    }
+  const handleSkinConcernsSelection = (selectedOptions) => {
+      const selectedValues = selectedOptions.map((option) => option.value);
+      setSkinConcerns(selectedValues);
+  }
+
+  const findRecommendation = () => {
+      const recommendations = recommendations.find((rec) => {
+          
+      })
+  }
    
   return (
     <div className='thanks-message'>
       {showThanks ? (
-        <div className='score-section'>
-          <h1>Thanks for answering</h1>
+        <div className="flex justify-center pt-12 pb-12">
+          <div className="block max-w-xl rounded-lg bg-white p-8 shadow-lg dark:bg-neutral-700">
+            <h1 className="mb-5 text-3xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+              Thanks for answering
+            </h1>
+          <div>
+            <iframe src="https://www.tiktok.com/embed/v2/7121944280456858926" frameborder="0" style={{maxWidth: '605px', minWidth: '325px', maxHeight: '605px', minHeight: '325px'}} ></iframe>
+          </div>
+          <h1 className="mb-5 text-3xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            Here's the suggested routine:
+          </h1>
+          </div>    
         </div>
   ) : (
         <div className="flex justify-center pt-12 pb-12">
@@ -119,7 +174,7 @@ export default function Quiz() {
             </h1>
           <div className='question-text'>{questions[currentQuestion].questionText}</div>
           <div className="flex p-8 grid grid-cols-2 gap-4 ">
-          {questions[currentQuestion].answerOptions.map((answerOption) => (
+            {questions[currentQuestion].answerOptions.map((answerOption) => (
               ( <label key={answerOption.id} >  
                   <button className="w-48 font-xl font-bold uppercase text-white py-3 px-10 font-bold text-xl uppercase bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-lg rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => handleAnswerOptionClick()}>
                     {answerOption.value}
@@ -128,10 +183,8 @@ export default function Quiz() {
               )
             ))}
           </div>
-
-            
-          </div>
-        </div>      
+        </div>
+      </div>        
       )}            
     </div>
   )
